f0373ac771f3afc3f987f22694264198170f7117
Patrick <biyagapatrick@gmail.com>
2019-05-07 16:12:48 +0100

    chatio

---
+++ server.js
@@ -0,0 +1,47 @@
+let express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+	usernames = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server Running...');
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket) => {
+	console.log('Socket Connected...');
+
+	socket.on('new user', (data, callback) => {
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	// Update Usernames
+	const updateUsernames = () => {
+		io.sockets.emit('usernames', usernames);
+	}
+
+	// Send Message
+	socket.on('send message', (data) => {
+		io.sockets.emit('new message', {msg: data, user:socket.username});
+	});
+
+	// Disconnect
+	socket.on('disconnect', (data) => {
+		if(!socket.username){
+			return;
+		}
+
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	});
+});
